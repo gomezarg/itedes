eb8caa3bcce89ccef02a6b58bf14c74b0538be30
JONIEL94 <gomez_arg@outlook.es>
2019-02-07 15:36:50 -0300

    Rent a car

---
+++ BaseDeDatos/persona.sql
@@ -0,0 +1,9 @@
+﻿-- create table persona (
+-- 	dni integer primary key,
+-- 	name text not null,
+-- 	surname text not null,
+-- 	birthday date,
+-- 	phone text
+-- )
+insert into persona values(38602057,'Jonathan','Gomez','1994-10-15','1165783160');
+select * from persona;
