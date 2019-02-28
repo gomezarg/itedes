a508dca51cedfe6dd71f8698f3a6f0ed73533759
JONIEL94 <gomez_arg@outlook.es>
2019-02-28 16:39:49 -0300

    Conexion a postgresql

--- modulo2/POO/PruebaPg/pruebapg/src/main/java/com/itedes/App.java
+++ modulo2/POO/PruebaPg/pruebapg/src/main/java/com/itedes/App.java
@@ -1,5 +1,6 @@
 package com.itedes;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 import java.sql.Connection;
@@ -8,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class App {
+	//
     private static Connection connection = null;
     private static Statement statement = null;
     private static ResultSet resultSet = null;
@@ -18,8 +20,8 @@ public class App {
 
         connection = DriverManager.getConnection (
         "jdbc:postgresql://localhost:5432/pedidos",
-        "dba", 
-        "1234"
+        "postgres", 
+        "root"
         );
 
         System.out.println("Conexion establecida");
@@ -75,7 +77,8 @@ public class App {
         System.out.println("ERROR AL CREAR CLIENTES");
         }
     }
-    //________
+
+    //Categoria
     public static void listCategorias() {
     try {
         resultSet = statement.executeQuery("SELECT * FROM categorias;");
@@ -93,6 +96,26 @@ public class App {
         }
 
     }
+	// detalle_ordenes
+	public static void listDetalle() {
+    try {
+        resultSet = statement.executeQuery("SELECT * FROM detalle_ordenes;");
+
+        while(resultSet.next()) {
+        Integer ordenid = resultSet.getInt("ordenid");
+		Integer detalleid = resultSet.getInt("detalleid");
+		Integer productoid = resultSet.getInt("productoid");
+		Integer cantidad = resultSet.getInt("cantidad");
+
+        System.out.println("ORDEN ID: " + ordenid + " | DETALLE ID:" + detalleid + " | PRODUCTO ID : " + productoid + " | CANTIDAD : " + cantidad);
+        }
+
+        resultSet.close();
+        } catch(Exception e) {
+        System.out.println("ERROR AL LISTAR DETALLES ORDENES");
+        }
+
+    }
 
 
     public static void addCategoria(Integer categoriaid, String nombrecat) {
@@ -106,7 +129,6 @@ public class App {
         }
     }
 
-
     public static void removeCategoria(Integer categoriaid) {
     try {
         String queryString = "DELETE FROM categorias WHERE categoriaid = " + categoriaid;
@@ -117,139 +139,559 @@ public class App {
         }
     }
 
-public static void main(String args[]) {
-    initializeConnection();
 
-    Scanner keyboard = new Scanner(System.in);
-    Integer opcion;
 
-    do {
-        System.out.println("Gestion de Categorias");
-        System.out.println("======= == ==========");
-        System.out.println();
-        System.out.println(" 1) Listar categorias");
-        System.out.println(" 2) Agregar categoria");
-        System.out.println(" 3) Eliminar categoria");
-        System.out.println("======= == ==========");
-        System.out.println("Gestion de CLIENTES");
-        System.out.println(" 4) Listar clientes");
-        System.out.println(" 5) Agregar clientes");
-        System.out.println(" 0) salir");
-        System.out.println();
-        System.out.print("Seleccione una opcion: ");
+//MAIN
+	public static void main(String[] args) {
+		initializeConnection();
 
-        try {
-            opcion = Integer.parseInt(keyboard.nextLine());
-        } catch(Exception e) {
-            System.out.println("Ingrese un numero valido");
-        opcion = -1;
-        continue;
-    }
+		Scanner keyboard = new Scanner(System.in);
+		
+		int opc=0;
 
-    Integer categoriaid;
-    Integer clienteid;
+		Integer categoriaid;
+		Integer clienteid;
+		//Integer ordenid , detalleid , productid , cantidad;
 
-    switch(opcion) {
-        case 1:
-            System.out.println("Listado de categorias:");
-            System.out.println("======= == ==========");
-            System.out.println();
+		do{
 
-            listCategorias();
+			System.out.println("Gestion de pedidos:");
+			System.out.println("__________________");
 
-        break;
+			System.out.println("1.CATEGORIAS");
+			System.out.println("2.CLIENTES");
+			System.out.println("3.DETALLE DE ORDENES");
+			System.out.println("4.EMPLEADOS");
+			System.out.println("5.ORDENES");
+			System.out.println("6.PRODUCTOS");
+			System.out.println("7.PROVEEDORES");
 
-        case 2:
-            System.out.println("Alta de categoria:");
-            System.out.println("==== == =========");
-            System.out.println("");
+			System.out.println("0.Salir");
 
-            System.out.print("Ingrese ID: ");
+			System.out.print("selecciona una opcion : ");
 
-            try {
-                categoriaid = Integer.parseInt(keyboard.nextLine());
-            } catch(Exception e) {
-                System.out.println("Error en el ingreso de datos");
-                continue;
-        }
-            System.out.print("Ingrese Nombre: ");
-            String nombrecat = keyboard.nextLine();
+			opc=keyboard.nextInt();
 
-            addCategoria(categoriaid, nombrecat);
+			
+			
+			
+			switch(opc){
 
-            break;
+			case 1:
+			
+				System.out.println("__________________");
+				System.out.println("Menu categorias");
+				System.out.println();
 
-        case 3:	
-            System.out.print("Ingrese ID: ");
+				int opc1=0;
 
-        try {
-            categoriaid = Integer.parseInt(keyboard.nextLine());
-        } catch(Exception e) {
-            System.out.println("Error en el ingreso de datos");
-            continue;
-        }
+				do{
 
-        removeCategoria(categoriaid);
+				System.out.println(" 1) Listar categorias");
+       			System.out.println(" 2) Agregar categoria");
+        		System.out.println(" 3) Eliminar categoria");
+				System.out.println(" 0) Salir");
 
-        case 4:
-            System.out.println("Listado de clientes:");
-            System.out.println("======= == ==========");
-            System.out.println();
+				System.out.print("selecciona una opcion : ");
 
-            listClientes();
+				opc1=keyboard.nextInt();
 
-        break;
-        case 5:
-            System.out.println("Alta de cliente:");
-            System.out.println("==== == =========");
-            System.out.println("");
+			
+			
+			switch(opc1){
+				
+				case 1:
+					System.out.println();
+					System.out.println("Listado de categorias:");
+					System.out.println();
+					listCategorias();
+				break;
 
-            System.out.print("Ingrese ID del cliente: ");
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de categoria");
+					System.out.println();
+					System.out.print("Ingrese ID: ");
+					try {
+                		categoriaid = Integer.parseInt(keyboard.nextLine());
+           			} catch(Exception e) {
+                		System.out.println("Error en el ingreso de datos");
+               			continue;
+        			}
+           			System.out.println("Ingrese Nombre: ");
+            		String nombrecat = keyboard.nextLine();
 
-            try {
-                clienteid = Integer.parseInt(keyboard.nextLine());
-            } catch(Exception e) {
-                System.out.println("Error en el ingreso de datos");
-                continue;
-        }
-            System.out.print("Ingrese cedula: ");
-            String cedula_ruc = keyboard.nextLine();
+            		addCategoria(categoriaid, nombrecat);
+				break;
 
-            System.out.print("Ingrese nombrecia: ");
-            String nombrecia = keyboard.nextLine();
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado de categoria");
+					System.out.println();
+					System.out.print("Ingrese ID: ");
 
-            System.out.print("Ingrese nombre de contacto: ");
-            String nombrecontacto = keyboard.nextLine();
+        			try {
+            			categoriaid = Integer.parseInt(keyboard.nextLine());
+        			} catch(Exception e) {
+           				System.out.println("Error en el ingreso de datos");
+            			continue;
+					}
+        			removeCategoria(categoriaid);
 
-            System.out.print("Ingrese direccion: ");
-            String direccioncli = keyboard.nextLine();
+				case 0:
+					System.out.println();
+					System.out.println("Saliendo de categorias");
+					System.out.println();
 
-            System.out.print("Ingrese fax: ");
-            String fax = keyboard.nextLine();
+				break;
 
-            System.out.print("Ingrese email: ");
-            String email = keyboard.nextLine();
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
 
-            System.out.print("Ingrese celular: ");
-            String celular = keyboard.nextLine();
+			
 
-            System.out.print("Ingrese fijo: ");
-            String fijo = keyboard.nextLine();
+			}while(opc1!=0);
 
-            addCliente(clienteid, cedula_ruc, nombrecia, nombrecontacto, direccioncli, fax, email, celular, fijo);
+			break;
 
-            break;
+			
 
-        case 0:
-            System.out.println("Saliendo...");
-        break;
+			case 2:
 
-        default:
-            System.out.println("Ingrese un numero valido");	
-        break;
-        }
-        } while(opcion != 0);
+				System.out.println("MENU CLIENTES:");
+				System.out.println();
+				int opc2=0;
 
-        closeConnection();
-    }
+			do{
+
+				System.out.println(" 1) Listar clientes");
+       			System.out.println(" 2) Agregar cliente");
+        		System.out.println(" 3) Eliminar cliente");
+				System.out.println(" 0) Salir");
+
+				System.out.print("selecciona una opcion : ");
+
+				opc2=keyboard.nextInt();
+
+				switch(opc2){
+
+				case 1:
+					System.out.println();
+					System.out.println("Listado de clientes:");
+            		System.out.println();
+
+            		listClientes();
+
+				break;
+
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de cliente");
+					System.out.println();
+					System.out.print("Ingrese ID del cliente: ");
+
+            	try {
+               	 	clienteid = Integer.parseInt(keyboard.nextLine());
+					} catch(Exception e) {
+						System.out.println("Error en el ingreso de datos");
+						continue;
+				}
+					System.out.print("Ingrese cedula: ");
+					String cedula_ruc = keyboard.nextLine();
+
+					System.out.print("Ingrese nombrecia: ");
+					String nombrecia = keyboard.nextLine();
+
+					System.out.print("Ingrese nombre de contacto: ");
+					String nombrecontacto = keyboard.nextLine();
+
+					System.out.print("Ingrese direccion: ");
+					String direccioncli = keyboard.nextLine();
+
+					System.out.print("Ingrese fax: ");
+					String fax = keyboard.nextLine();
+
+					System.out.print("Ingrese email: ");
+					String email = keyboard.nextLine();
+
+					System.out.print("Ingrese celular: ");
+					String celular = keyboard.nextLine();
+
+					System.out.print("Ingrese fijo: ");
+					String fijo = keyboard.nextLine();
+
+					addCliente(clienteid, cedula_ruc, nombrecia, nombrecontacto, direccioncli, fax, email, celular, fijo);
+
+				
+
+				break;
+
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado de cliente");
+					System.out.println();
+
+
+				break;
+
+				case 0:
+					System.out.println();
+					System.out.println(" Saliendo de clientes");
+					System.out.println();
+
+				break;
+
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
+
+			}while(opc2 != 0);
+
+			break;
+			case 3:
+				System.out.println("__________________");
+				System.out.println("Menu detalle de ordenes:");
+				System.out.println();
+
+				int opc3=0;
+
+				do{
+
+				System.out.println(" 1) Listar ordenes");
+       			System.out.println(" 2) Agregar ordenes");
+        		System.out.println(" 3) Eliminar ordenes");
+				System.out.println(" 0) Salir");
+
+				System.out.print("selecciona una opcion : ");
+
+				opc3=keyboard.nextInt();
+
+			
+
+			switch(opc3){
+
+				case 1:
+					System.out.println();
+					System.out.println("Listado de detalle de ordenes:");
+					System.out.println();
+
+            		listDetalle();
+				break;
+
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de detalles de ordenes");
+					System.out.println();
+
+				
+
+				break;
+
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado detalle de ordenes");
+					System.out.println();
+
+
+				break;
+
+				case 0:
+					System.out.println();
+					System.out.println(" Saliendo de detalle de ordenes");
+					System.out.println();
+
+				break;
+
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
+
+			
+
+			}while(opc3!=0);
+
+
+			break;
+
+			case 4:
+				System.out.println("__________________");
+				System.out.println("MENU DE EMPLEADOS:");
+				System.out.println();
+
+				int opc4=0;
+
+				do{
+
+				System.out.println(" 1) Listar empleados");
+       			System.out.println(" 2) Agregar empleados");
+        		System.out.println(" 3) Eliminar empleados");
+				System.out.println(" 0) Salir");
+
+				System.out.print("selecciona una opcion : ");
+
+				opc4=keyboard.nextInt();
+
+			
+
+			switch(opc4){
+
+				case 1:
+					System.out.println();
+					System.out.println("Listado de empleados:");
+					System.out.println();
+
+					
+
+				break;
+
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de empleados");
+					System.out.println();
+
+				
+
+				break;
+
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado de empleados");
+					System.out.println();
+
+
+				break;
+
+				case 0:
+					System.out.println();
+					System.out.println(" Saliendo del menu empleados");
+					System.out.println();
+
+				break;
+
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
+
+			
+
+			}while(opc4!=0);
+
+			break;
+
+			case 5:
+				System.out.println("__________________");
+				System.out.println("MENU DE ORDENES:");
+				System.out.println();
+
+				int opc5=0;
+
+				do{
+
+				System.out.println(" 1) Listar ordenes");
+       			System.out.println(" 2) Agregar ordenes");
+        		System.out.println(" 3) Eliminar ordenes");
+				System.out.println(" 0) Salir");
+
+				System.out.print("selecciona una opcion : ");
+
+				opc5=keyboard.nextInt();
+
+			
+
+			switch(opc5){
+
+				case 1:
+					System.out.println();
+					System.out.println("Listado de ordenes:");
+					System.out.println();
+
+					
+
+				break;
+
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de ordenes");
+					System.out.println();
+
+				
+
+				break;
+
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado de ordenes");
+					System.out.println();
+
+
+				break;
+
+				case 0:
+					System.out.println();
+					System.out.println(" Saliendo del menu ordenes");
+					System.out.println();
+
+				break;
+
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
+
+			
+
+			}while(opc5!=0);
+			break;
+
+			case 6 :
+				System.out.println("__________________");
+				System.out.println("MENU DE PRODUCTOS:");
+				System.out.println();
+
+				int opc6=0;
+
+				do{
+
+				System.out.println(" 1) Listar productos");
+       			System.out.println(" 2) Agregar producto");
+        		System.out.println(" 3) Eliminar producto");
+				System.out.println(" 0) Salir");
+
+				System.out.print("selecciona una opcion : ");
+
+				opc6=keyboard.nextInt();
+
+			
+
+			switch(opc6){
+
+				case 1:
+					System.out.println();
+					System.out.println("Listado de productos:");
+					System.out.println();
+
+					
+
+				break;
+
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de productos");
+					System.out.println();
+
+				
+
+				break;
+
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado de productos");
+					System.out.println();
+
+
+				break;
+
+				case 0:
+					System.out.println();
+					System.out.println(" Saliendo del menu productos");
+					System.out.println();
+
+				break;
+
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
+
+			
+
+			}while(opc6!=0);
+			break;
+			case 7:
+				System.out.println("__________________");
+				System.out.println("MENU DE PROVEEDORES:");
+				System.out.println();
+
+				int opc7=0;
+
+				do{
+				System.out.println();
+				System.out.println(" 1) Listar proveedores");
+       			System.out.println(" 2) Agregar proveedor");
+        		System.out.println(" 3) Eliminar proveedor");
+				System.out.println(" 0) Salir");
+
+				System.out.print("selecciona una opcion : ");
+
+				opc7=keyboard.nextInt();
+
+			
+
+			switch(opc7){
+
+				case 1:
+					System.out.println();
+					System.out.println("Listado de proveedores:");
+					System.out.println();
+
+					
+
+				break;
+
+				case 2:
+					System.out.println();
+					System.out.println("Agregado de proveedores");
+					System.out.println();
+
+				
+
+				break;
+
+				case 3:
+					System.out.println();
+					System.out.println("Eliminado de proveedores");
+					System.out.println();
+
+
+				break;
+
+				case 0:
+					System.out.println();
+					System.out.println(" Saliendo del menu proveedores");
+					System.out.println();
+
+				break;
+
+				default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+				}
+
+			
+
+			}while(opc7!=0);
+			break;
+			default:
+					System.out.println();
+					System.out.println("Opcion invalida");
+					System.out.println();
+			}
+
+		} while(opc != 0);
+		closeConnection();
+
+	}
+
 }
